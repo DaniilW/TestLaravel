e402d569b1a2f5ce1fbe6c85831b71a69318c8e3
WANT <want@DESKTOP-4F838O2.localdomain>
2024-01-06 15:11:30 +0300

    первая фиксация

---
+++ example-app/app/Http/Controllers/MyPlaceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyPlaceController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
---
+++ quasar/src/pages/IndexPage2.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/ExampleComponent.vue';
+import { ref } from 'vue';
+
+const todos = ref<Todo[]>([
+  {
+    id: 1,
+    content: 'ct1',
+  },
+
+]);
+const meta = ref<Meta>({
+  totalCount: 100,
+});
+</script>
